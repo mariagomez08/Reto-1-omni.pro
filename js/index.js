5b8cf66a2c0fa3aef3c792625d0dc3ee91da7678
[mariagomez08] <[mariiangelgomezsg@gmail.com]>
2022-08-08 18:58:17 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,63 +1,13 @@
-(function ($) {
-    "use strict";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
-    /*[ Select 2 Config ]
-          ===========================================================*/
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  })
 
-    try {
-        var selectSimple = $(".js-select-simple");
-
-        selectSimple.each(function () {
-            var that = $(this);
-            var selectBox = that.find("select");
-            var selectDropdown = that.find(".select-dropdown");
-            selectBox.select2({
-                dropdownParent: selectDropdown,
-            });
-        });
-    } catch (err) {
-        console.log(err);
-    }
-})(jQuery);
-
-function valida_envia() {
-    //valido el nombre
-
-
-    if (document.formulario.inputNombre.value.length == 0) {
-        alert("Tiene que ingresar su nombre");
-        document.formulario.inputNombre.focus();
-        return 0;
-    }
-    if (document.formulario.inputCiudad.value.length == 0) {
-        alert("Tiene que ingresar la ciudad");
-        document.formulario.inputCiudad.focus();
-        return 0;
-    }
-    if (document.formulario.inputEdad.value.length == 0) {
-        alert("Tiene que ingresar la Edad");
-        document.formulario.inputEdad.focus();
-    }  
-    if (document.formulario.inputCategoria.selectedIndex == 0) {
-        alert("Tiene que ingresar la categoría");
-        document.formulario.inputCategoria.focus();
-        return 0;
-    }
-    if (document.formulario.inputEmail.value.length == 0) {
-        alert("Tiene que ingresar su correo electronico");
-        document.formulario.inputEmail.focus();
-        return 0;
-    }
-    if (document.formulario.inputNumero.value.length == 0) {
-        alert("Tiene que ingresar el numero de telefono");
-        document.formulario.inputNumero.focus();
-        return 0;
-    }
-
-    //el formulario se envia
-    alert("Muchas gracias por enviar el formulario");
-    document.formulario.submit();
-}  
 
 
 
---
+++ js/sweetAlert.js
@@ -0,0 +1,5 @@
+Swal.fire({
+    title: 'Bienvenido',
+    text: 'Si te registras hoy tendras un 10% de descuento durante el primer mes en envios',
+    confirmButtonText: 'Ok',
+  })
