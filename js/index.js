272549667b1d51c11206acbce8d9a604b8071e87
[mariagomez08] <[mariiangelgomezsg@gmail.com]>
2022-08-08 16:30:36 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,48 +1,62 @@
 (function ($) {
-    'use strict';
-    
-    
+    "use strict";
+
     /*[ Select 2 Config ]
-        ===========================================================*/
-    
+          ===========================================================*/
+
     try {
-        var selectSimple = $('.js-select-simple');
-    
+        var selectSimple = $(".js-select-simple");
+
         selectSimple.each(function () {
             var that = $(this);
-            var selectBox = that.find('select');
-            var selectDropdown = that.find('.select-dropdown');
+            var selectBox = that.find("select");
+            var selectDropdown = that.find(".select-dropdown");
             selectBox.select2({
-                dropdownParent: selectDropdown
+                dropdownParent: selectDropdown,
             });
         });
-    
     } catch (err) {
         console.log(err);
     }
-    
-
 })(jQuery);
 
-function valida_envia(){
+function valida_envia() {
     //valido el nombre
-    if (document.formulario.inputNombre.value.length==0){
-           alert("Tiene que ingresar su nombre")
-           document.formulario.inputNombre.focus()
-           return 0;
-    }
-    if (document.formulario.inputCiudad.value.length==0){
-        alert("Tiene que ingresar la ciudad")
-        document.formulario.inputCiudad.focus()
+
+
+    if (document.formulario.inputNombre.value.length == 0) {
+        alert("Tiene que ingresar su nombre");
+        document.formulario.inputNombre.focus();
         return 0;
- }
- 
+    }
+    if (document.formulario.inputCiudad.value.length == 0) {
+        alert("Tiene que ingresar la ciudad");
+        document.formulario.inputCiudad.focus();
+        return 0;
+    }
+    if (document.formulario.inputEdad.value.length == 0) {
+        alert("Tiene que ingresar la Edad");
+        document.formulario.inputEdad.focus();
+    }  
+    if (document.formulario.inputCategoria.selectedIndex == 0) {
+        alert("Tiene que ingresar la categoría");
+        document.formulario.inputCategoria.focus();
+        return 0;
+    }
+    if (document.formulario.inputEmail.value.length == 0) {
+        alert("Tiene que ingresar su correo electronico");
+        document.formulario.inputEmail.focus();
+        return 0;
+    }
+    if (document.formulario.inputNumero.value.length == 0) {
+        alert("Tiene que ingresar el numero de telefono");
+        document.formulario.inputNumero.focus();
+        return 0;
+    }
+
     //el formulario se envia
     alert("Muchas gracias por enviar el formulario");
     document.formulario.submit();
 }
 
-    //valido la edad. tiene que ser entero mayor que 18
-  
- 
-  
+//valido la edad. tiene que ser entero mayor que 18
